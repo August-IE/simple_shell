02d448f7e6b8f457cc81a9643071e2f7f4bc4756
August-IE <aeromosele9@gmail.com>
2023-08-20 13:14:10 -0700

    implemented string functions

--- Strings_functions.c
+++ Strings_functions.c
@@ -5,9 +5,6 @@ char *_strchr(char *s, char c);
 char *_strcpy(char *dest, char *src);
 unsigned int _strspn(char *s, char *accept);
 char *_strncpy(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
 
 /**
  * _strlen - Returns the length of a string.
@@ -127,72 +124,4 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[index] = '\0';
 
 	return (dest);
-}
-
-
-/**
- * _strcat - Concatenates the string pointed to by @src, including
- * the terminating null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- *
- * Return: A pointer to the destination string @dest.
- */
-char *_strcat(char *dest, char *src)
-{
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
-}
-
-
-/**
- * _strncat - Concatenates two strings using at most
- *            an inputted number of bytes from src.
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
- *
- * Return: A pointer to the resulting string dest.
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
-}
-
-
-
-/**
- * _strcmp - Compares pointers to two strings.
- * @s1: A pointer to the first string to be compared.
- * @s2: A pointer to the second string to be compared.
- *
- * Return: If str1 < str2, the negative difference of the unmatched characters
- *
- *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the unmatched characters.
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
 }
