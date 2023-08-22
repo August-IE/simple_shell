0ca7bd3e97a07636a1ad572385c09c32d2e6e92e
Emedo586 <emedopeace99@gmaiil.com>
2023-08-22 15:45:48 -0700

    update

---
+++ memory.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * *_realloc -
+ * @ptr:
+ * @old_size:
+ * @new_size:
+ *
+ * Return:
+ */
+void *_realloc()
+{
+
+}
+
+/**
+ * *_memcpy - copies the memory area
+ * @dest: destination of new ptr to the memory area.
+ * @src: source pointer to the memory area
+ * @size: size of the memory area.
+ * Return:
+ *
+ */
+void *memcpy(char *dest, char *ptr, unsigned int size)
+{
+unsigned int i;
+
+for (i = 0; i < size; i++)
+src[i] = dest[i];
+return (dest);
+}
