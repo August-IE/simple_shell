c2b238df215c641d1c249bcaa0b182ca25082680
August-IE <aeromosele9@gmail.com>
2023-08-22 02:41:26 -0700

    all c functions used

---
+++ linked_lists.c
@@ -0,0 +1,152 @@
+#include "shell.h"
+#include <stdio.h>
+
+/**
+ * print_list - A function that prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+
+
+/**
+ * list_len - A function that returns the number of elements in a linked
+ *		list_t
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
+
+
+/**
+ * add_node - A function that adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ * Return: NULL - If the function fails
+ *         Otherwise - the address of the new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
+
+
+
+
+/**
+ * add_node_end - A function that adds a new node at the end of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ * Return: NULL - If the function fails
+ *         Otherwise - the address of the new element.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new, *last;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+
+	return (*head);
+}
+
+
+
+/**
+ * free_list - A function that frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
--- shell.h
+++ shell.h
@@ -32,7 +32,6 @@ char *location(char *path, char *arg);
 char *get_loc(char *arg);
 int _builtInCmd(char **arg);
 
-
 /******GLOBAL VARIABLES*******/
 extern char **environ;
 
@@ -65,5 +64,28 @@ char **_strtok(char *line, char *delim);
 char *get_location(char *command);
 int execute(char **args, char **front);
 char *_itoa(int num);
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /*_SHELL_H_ */
