ebb37c393f661ed43e1f553fd503942eabb4db6d
Emedo586 <emedopeace99@gmaiil.com>
2023-08-23 04:36:41 -0700

    command upadated

--- command_prompt.c
+++ command_prompt.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv, char **env)
 char input[MAX_INPUT_SIZE];
 char *args[MAX_INPUT_SIZE]; /* The arguments to be passed to execve */
 pid_t pid;
+int status;
 (void)argc;
 (void)argv;
 (void)env;
@@ -22,7 +23,6 @@ while (1)
 if (isatty(STDIN_FILENO))
 { printf("AC_Shell >> ");
 fflush(stdout); }
-
 {
 if (fgets(input, sizeof(input), stdin) == NULL)
 {
@@ -32,31 +32,21 @@ break; /* End of file (Ctrl+D) */ }
 else
 { perror("Input error");
 continue; }}}
-
 input[strcspn(input, "\n")] = '\0';
-
 if (strcmp(input, "exit") == 0)
-{
 printf("Exiting shell...\n");
-break; }
-
+break;
 pid = fork();
-
 if (pid == 0)
 {/* Child process */ char *env[] = { NULL }; /* Empty environment for execve */
 args[0] = input;
 args[1] = NULL;
-
-{
 if (execve(input, args, env) == -1)
 { perror("Command execution error");
-exit(1); }}}
+exit(1); }}
 else if (pid < 0)
-{ perror("Fork failed"); }
-else
-/* Parent process */
-{ int status;
-waitpid(pid, &status, 0); }}
-
+perror("Fork failed");
+else /* Parent process */
+waitpid(pid, &status, 0); }
 return (0);
 }
