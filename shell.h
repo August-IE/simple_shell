f779f6c9eca5937b1b58aef6a1f87031842571bb
Emedo586 <emedopeace99@gmaiil.com>
2023-08-22 17:07:37 -0700

    updated

--- shell.h
+++ shell.h
@@ -54,7 +54,7 @@ void set_string(char **s, char *to);
 /*void display_prompt(void);*/
 
 /******FOR MEMORY******/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void **ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int size);
 char **_reallocdp(char *ptr, unsigned int old_size, unsigned int new_size);
 
