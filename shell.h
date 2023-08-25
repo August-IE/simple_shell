b5f40d7f1b13e12f68452bbc33d72d226bf1b32c
Emedo586 <emedopeace99@gmaiil.com>
2023-08-25 01:11:21 -0700

    another update

--- shell.h
+++ shell.h
@@ -20,19 +20,15 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
-<<<<<<< HEAD
 #include <limits.h>
-=======
 #include <stdarg.h>
->>>>>>> refs/remotes/origin/master
 
 #define MAX_INPUT_SIZE 1024
 #define END_OF_FILE -2
 #define EXIT -3
 
-<<<<<<< HEAD
-=======
-/*************************FUNCTION PROTOTYPES************************/
+
+/*************************FUNCTION PROTOTYPES*********************
 int _putchar(char c);
 int _printstring(char *str);
 char *location(char *path, char *arg);
@@ -42,9 +38,7 @@ int main(int argc, char **argv);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
->>>>>>> refs/remotes/origin/master
-
+void print_all(const char * const format, ...)**/
 /**
  * struct ACdata
  * @argv:
@@ -53,7 +47,6 @@ void print_all(const char * const format, ...);
  * @status:
  * @**_environ:
  * @pid:
-*/
 typedef struct ACdata
 {
 char **argv;
@@ -63,18 +56,17 @@ char **_environ;
 char pid;
 int status;
 int count;
-} ACdata_t;
+} ACdata_t;**/
 
 /**
  * struct builtin_s
  * @name:
  * @f:
-*/
 typedef struct builtin_s
 {
 char *name;
 int (*f)(ACdata_t *acdata);
-} builtin_t;
+} builtin_t;**/
 
 /**
  * struct list_s - singly linked list
@@ -95,46 +87,43 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head)
+void free_list(list_t *head);
 
 /*************************FUNCTION PROTOTYPES************************/
 
-<<<<<<< HEAD
-/*********GETLINE**********/
+/*********GETLINE*********
 void bring_line(char **lineptr, size_t *n, char *buffer, size_t i);
-ssize_t get_livoidne(char **lineptr, size_t *n, FILE *stream);
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
 char *read_line(int *i_eof);
 
-/**ENVIRONMENT VARIABLRES**/
+*ENVIRONMENT VARIABLRES**
 char *_getenv(const char *name, char **_environ);
-int env(ACdata_t *acdata);
+int _env(ACdata_t *acdata);
 
-/******CD SHELL*******/
+*****CD SHELL******
 void cd_dot(ACdata_t *acdata);
 void cd_to(ACdata_t *acdata);
 void cd_previous(ACdata_t *acdata);
 void cd_home(ACdata_t *acdata);
 int cd_shell(ACdata_t *acdata);
+******EXIT SHELL*******
+int exit_shell(ACdata_t *acdata);**/
 
-/********EXIT SHELL*********/
-int exit_shell(ACdata_t *acdata);
-
-/***********BUILTIN_CMD***********/
+/***********BUILTIN_CMD**********
 int (*get_builtin(char *cmd))(ACdata_t *acdata);
 char *copy_data(char *name, char *n);
 void set_env(char *name, char *n, ACdata_t *acdata);
 int _setenv(ACdata_t *acdata);
-int _unsetenv(ACdata_t *acdata);
+int _unsetenv(ACdata_t *acdata);**/
 
-/*********STRING_FUNCTIONS3*********/
+/*********STRING_FUNCTIONS3********
 char *_strdup(const char *s);
 int _isdigit(const char *s);
 void rev_string(char *s);
-int _strlen(char *s);
-=======
+int _strlen(char *s);**/
+
 /***STRING_FUNCTIONS***/
 int _strlen(const char *s);
->>>>>>> refs/remotes/origin/master
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -153,13 +142,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 
-<<<<<<< HEAD
-=======
-/**GETENV**/
-char **_getenv(const char *env);
-
-
->>>>>>> refs/remotes/origin/master
 /* Main Helpers */
 char **_strtok(char *line, char *delim);
 char *get_location(char *command);
@@ -171,29 +153,7 @@ int _printstring(char *str);
 char *location(char *path, char *arg);
 char *get_loc(char *arg);
 int _builtInCmd(char **arg);
-int main(int argc, char **argv, char **env);
-
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
+/*int main(int argc, char **argv, char **env);*/
 
 /***** VARIADIC FUNCTIONS *****/
 
@@ -209,24 +169,6 @@ typedef struct printer
 	void (*print)(va_list arg);
 
 } printer_t;
-
-#endif /*_SHELL_H_ */
-
-
-
-
-
-#ifndef SHELL_H
-#define SHELL_H
-
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 /**Error printing functions */
 void zerror(char *prog, int count, char *cmd);
