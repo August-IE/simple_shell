fd0fefff0e6443bb2cae8e31a98e52aa7da9b627
Emedo586 <emedopeace99@gmaiil.com>
2023-08-25 06:44:48 -0700

    one last push

--- shell.h
+++ shell.h
@@ -27,50 +27,6 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
-<<<<<<< HEAD
-
-/*************************FUNCTION PROTOTYPES*********************
-int _putchar(char c);
-int _printstring(char *str);
-char *location(char *path, char *arg);
-char *get_loc(char *arg);
-int _builtInCmd(char **arg);
-int main(int argc, char **argv);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...)**/
-/**
- * struct ACdata
- * @argv:
- * @input:
- * @args:
- * @status:
- * @**_environ:
- * @pid:
-typedef struct ACdata
-{
-char **argv;
-char **args;
-char *input;
-char **_environ;
-char pid;
-int status;
-int count;
-} ACdata_t;**/
-
-/**
- * struct builtin_s
- * @name:
- * @f:
-typedef struct builtin_s
-{
-char *name;
-int (*f)(ACdata_t *acdata);
-} builtin_t;**/
-
-=======
->>>>>>> 24f5c3490d5b85ffc2d968ebc0bd5794ab70dc5e
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -92,44 +48,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-<<<<<<< HEAD
-/*************************FUNCTION PROTOTYPES************************/
-
-/*********GETLINE*********
-void bring_line(char **lineptr, size_t *n, char *buffer, size_t i);
-ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
-char *read_line(int *i_eof);
-
-*ENVIRONMENT VARIABLRES**
-char *_getenv(const char *name, char **_environ);
-int _env(ACdata_t *acdata);
-
-*****CD SHELL******
-void cd_dot(ACdata_t *acdata);
-void cd_to(ACdata_t *acdata);
-void cd_previous(ACdata_t *acdata);
-void cd_home(ACdata_t *acdata);
-int cd_shell(ACdata_t *acdata);
-******EXIT SHELL*******
-int exit_shell(ACdata_t *acdata);**/
-
-/***********BUILTIN_CMD**********
-int (*get_builtin(char *cmd))(ACdata_t *acdata);
-char *copy_data(char *name, char *n);
-void set_env(char *name, char *n, ACdata_t *acdata);
-int _setenv(ACdata_t *acdata);
-int _unsetenv(ACdata_t *acdata);**/
-
-/*********STRING_FUNCTIONS3********
-char *_strdup(const char *s);
-int _isdigit(const char *s);
-void rev_string(char *s);
-int _strlen(char *s);**/
-
 /***STRING_FUNCTIONS***/
-=======
- /***STRING_FUNCTIONS***/
->>>>>>> 24f5c3490d5b85ffc2d968ebc0bd5794ab70dc5e
 int _strlen(const char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
@@ -161,10 +80,7 @@ char *location(char *path, char *arg);
 char *get_loc(char *arg);
 int _builtInCmd(char **arg);
 /*int main(int argc, char **argv, char **env);*/
-<<<<<<< HEAD
-=======
 
->>>>>>> 24f5c3490d5b85ffc2d968ebc0bd5794ab70dc5e
 
 /***** VARIADIC FUNCTIONS *****/
 
